31bef2a27cb01f4571c048f736fe3dd3665ef2e0
Gassan Idriss <ghassani@gmail.com>
2017-01-19 16:54:56 -0800

    f

--- packet.h
+++ packet.h
@@ -13,34 +13,82 @@ namespace OpenPST {
         	};
 
         	protected:
-        		PacketEndianess endianT = kPacketEndianessLittle;
-        		PacketEndianess endianH = kPacketEndianessLittle;
-
+        		PacketEndianess endianT;
+        		PacketEndianess endianH;
+        		Packet* response = nullptr;
+        		bool sent = false;
             public:
                 
-                Packet(){
+                Packet(PacketEndianess endianT = kPacketEndianessLittle, PacketEndianess endianH = kPacketEndianessLittle) : 
+                	endianT(endianT), 
+                	endianH(endianH) 
+                {
 
                 }
                 
                 ~Packet(){
-
+                	if (response != nullptr) {
+                		delete response;
+                	}
                 }
                 
                 void setTargetEndianess(PacketEndianess e) {
                 	endianT = e;
                 }
 
+                /**
+                *
+                */
                 PacketEndianess getTargetEndianess() {
                 	return endianT;
                 }
 
+                /**
+                * @brief Set the host endianess
+                *
+                * @param PacketEndianess e
+                * @return void
+                */
                 void setHostEndianess(PacketEndianess e) {
                 	endianH = e;
                 }
 
+                /**
+                * @brief Get the host endianess
+                *
+                * @return PacketEndianess
+                */
                 PacketEndianess getHostEndianess() {
                 	return endianH;
                 }
+
+                /**
+                *
+                */
+                bool isSent() {
+                	return sent;
+                }
+
+                /**
+                *
+                */
+                void setSent(bool s) {
+                	sent = s;
+                }
+
+                u16 flip(uint16_t i);
+                u32 flip(uint32_t i);
+                u64 flip(uint64_t i);
+                /**
+                *
+                */
+                //virtual std::vector<uint8_t> pack() = 0;
+               
+               /**
+                *
+                */
+                //virtual Packet* unpack(std::vector<uint8_t>& data) = 0;
+ 
         };
     }
 }
